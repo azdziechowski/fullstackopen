1ac299f64a7a5a5b2779ece13582853cbdd9ea2e
kct949 <kct949@alumni.ku.dk>
2024-08-02 22:56:42 +0200

    add unicafe 1.6 - 1.11

--- part1/exercise1/src/App.jsx
+++ part1/exercise1/src/App.jsx
@@ -1,25 +1,58 @@
-import { useState } from "react";
+import { useState } from 'react'
 
-const Display = ({counter}) => <div>{counter}</div>
-const Button = ({text, onClick}) => <button onClick={onClick}> {text} </button>
+const Button = ({text, onClick}) =>
+  {
+    return (
+        <button onClick={onClick}>{text}</button>
+    )
+  }
+
+const Statistics = ({good, neutral, bad}) => {
+  if (good + neutral + bad === 0) {
+    return <div>No feedback provided yet</div>
+  }
+
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text='good' acc={good} />
+        <StatisticLine text='neutral' acc={neutral} />
+        <StatisticLine text='bad' acc={bad} />
+        <StatisticLine text='all' acc={good + neutral + bad} />
+        <StatisticLine text='avg' acc={(good - bad)/(good + neutral + bad)} />
+        <StatisticLine text='pos' acc={good/(good + neutral + bad)} />
+      </tbody>
+    </table>
+  )
+}
+
+  
+const StatisticLine = ({text, acc}) => {
+  return (  
+    <tr>
+      <td>{text}</td> 
+      <td>{acc}</td>
+    </tr>
+  )
+}
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  const incrementCounter = () => setCounter(counter + 1);
-  
-  const decrementCounter = () => counter > 0 
-    ? setCounter(counter - 1) 
-    : setCounter(counter);
-
-  const resetCounter = () => setCounter(0);
+  const handleClick = (acc, stateFunc) => () => stateFunc(acc + 1);
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button text='increment' onClick={incrementCounter} />
-      <Button text='decrement' onClick={decrementCounter} />
-      <Button text='reset' onClick={resetCounter} />
+    <h1>Leave your feedback</h1>
+    <Button text='good' onClick={handleClick(good, setGood)}  />
+    <Button text='neutral' onClick={handleClick(neutral, setNeutral)}  /> 
+    <Button text='bad' onClick={handleClick(bad, setBad)}  />
+    
+    <h1>stats</h1>
+    <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
