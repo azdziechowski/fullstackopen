e9fe6e01bc3b6f78f7aec36d0e2116d78bb62121
kct949 <kct949@alumni.ku.dk>
2024-08-03 09:50:54 +0200

    fix the folder structure

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const Display = ({counter}) => <div>{counter}</div>
+const Button = ({text, onClick}) => <button onClick={onClick}> {text} </button>
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const incrementCounter = () => setCounter(counter + 1);
+
+  const decrementCounter = () => counter > 0 
+    ? setCounter(counter - 1) 
+    : setCounter(counter);
+
+  const resetCounter = () => setCounter(0);
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button text='increment' onClick={incrementCounter} />
+      <Button text='decrement' onClick={decrementCounter} />
+      <Button text='reset' onClick={resetCounter} />
+    </div>
+  )
+}
+export default App
--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,34 +1,59 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Button = ({text, onClick}) =>
+  {
+    return (
+        <button onClick={onClick}>{text}</button>
+    )
+  }
+
+const Statistics = ({good, neutral, bad}) => {
+  if (good + neutral + bad === 0) {
+    return <div>No feedback provided yet</div>
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <table>
+      <tbody>
+        <StatisticLine text='good' acc={good} />
+        <StatisticLine text='neutral' acc={neutral} />
+        <StatisticLine text='bad' acc={bad} />
+        <StatisticLine text='all' acc={good + neutral + bad} />
+        <StatisticLine text='avg' acc={(good - bad)/(good + neutral + bad)} />
+        <StatisticLine text='pos' acc={good/(good + neutral + bad)} />
+      </tbody>
+    </table>
+  )
+}
+
+  
+const StatisticLine = ({text, acc}) => {
+  return (  
+    <tr>
+      <td>{text}</td> 
+      <td>{acc}</td>
+    </tr>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleClick = (acc, stateFunc) => () => stateFunc(acc + 1);
+
+  return (
+    <div>
+    <h1>Leave your feedback</h1>
+    <Button text='good' onClick={handleClick(good, setGood)}  />
+    <Button text='neutral' onClick={handleClick(neutral, setNeutral)}  /> 
+    <Button text='bad' onClick={handleClick(bad, setBad)}  />
+    
+    <h1>stats</h1>
+    <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
   )
 }
 
